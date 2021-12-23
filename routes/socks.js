4e0fd29110bacae13f396adc20c06cd5be24b3a4
Kiaya Anderson <kiayaanderson@Kiayas-MacBook-Air.local>
2021-12-23 09:50:45 -0500

    updated routes

--- routes/socks.js
+++ routes/socks.js
@@ -4,5 +4,9 @@ import * as controllers from "../controllers/socks.js";
 const router = Router();
 
 router.get("/socks", controllers.getSocks);
+router.get("/socks/:id", controllers.getSock)
+router.post("/socks", restrict, controllers.createSock)
+router.put("/socks/:id", restrict, controllers.updateSock)
+router.delete('/socks/:id', restrict, controllers.deleteSock)
 
 export default router;
