e9bc477a0797b99ee3036c60776372395122223f
Akindele Akinfenwa <Dele@Akindeles-MacBook-Pro.local>
2021-12-23 11:19:13 -0500

    imported restrict to routes/socks.js

--- routes/socks.js
+++ routes/socks.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as controllers from "../controllers/socks.js";
+import restrict from "../helpers/restrict.js";
 
 const router = Router();
 
 router.get("/socks", controllers.getSocks);
-router.get("/socks/:id", controllers.getSock)
-router.post("/socks", restrict, controllers.createSock)
-router.put("/socks/:id", restrict, controllers.updateSock)
-router.delete('/socks/:id', restrict, controllers.deleteSock)
+router.get("/socks/:id", controllers.getSock);
+router.post("/socks", restrict, controllers.createSock);
+router.put("/socks/:id", restrict, controllers.updateSock);
+router.delete("/socks/:id", restrict, controllers.deleteSock);
 
 export default router;
