4551577d786e2b5de3c47d9a873a0e74c8f95c4f
Nitesh <nitlmisra@hotmail.com>
2022-01-03 14:28:32 -0500

    Changes to nav

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Layout.css'
 import Header from '../Header/Header'
 // import Footer from '../Footer/Footer'
-// import Nav from '../Nav/Nav'
+import Nav from '../Nav/Nav'
 
 export default function Layout(props) {
   return (
