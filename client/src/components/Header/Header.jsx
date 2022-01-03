729ce1621dd96174fcc92d227bfed48438e64c22
Kiaya Anderson <kiayaanderson@Kiayas-MacBook-Air.local>
2022-01-03 14:21:58 -0500

    updated files

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./Header.css"
 
 export default function Header() {
   return (
-    <div>
-      <h1>PAIN</h1>
+    <div className='header-img'>
+      <img src="https://res.cloudinary.com/kacloud20/image/upload/v1640200935/project3/Sock2%20Photos/Screen_Shot_2021-12-21_at_1.57.34_PM_f2vfpv.png"
+        alt="top-design" width="100%" />
     </div>
   )
 }
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import './Layout.css'
 import Header from '../Header/Header'
 // import Footer from '../Footer/Footer'
-// import Nav from '../Nav/Nav'
+import Nav from '../Nav/Nav'
 
 export default function Layout(props) {
   return (
     <div className='layout'>
       <Header />
       <Nav user={props.user} />
-      {/* <div className='layout-children'>
+       <div className='layout-children'>
         {props.children}
       </div>
-      <Footer /> */}
+      {/* <Footer />  */}
     </div>
   )
 }
