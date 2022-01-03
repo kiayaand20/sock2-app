4c063269cf02ed495e0d21e73c59fbdad54a3c61
Kiaya Anderson <kiayaanderson@Kiayas-MacBook-Air.local>
2022-01-03 12:24:59 -0500

    added info to sock cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css';
-// import Layout from './components/Layout/Layout';
 import Home from './screens/Home/Home';
 import {Routes, Route} from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
-      <h1>hello</h1>
     </div>
   );
 }
