b44bd71b8a06e7b42c200d2c9064d4c3b722c9d6
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2022-01-03 15:02:42 -0600

    product-create

--- client/src/components/Add/SockAdd.jsx
+++ client/src/components/Add/SockAdd.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './SockAdd.css'
-import { Layout } from '../../components'
+import Layout from '../Layout/Layout'
 import { useNavigate } from 'react-router-dom'
 import { createSock } from '../../services/socks'
 
