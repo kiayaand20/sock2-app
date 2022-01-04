79c54be93a476590571905652807b690f9ed881d
Kiaya Anderson <kiayaanderson@Kiayas-MacBook-Air.local>
2022-01-04 11:43:39 -0500

    updated sock.jsx file in screens

--- client/src/screens/AllSocks/AllSocks.jsx
+++ client/src/screens/AllSocks/AllSocks.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './AllSocks.css'
 import Layout from '../../components/Layout/Layout'
 import SockCards from '../../components/SockCards/SockCards'
-// import { Link } from 'react-router-dom'
 
 export default function AllSocks(props) {
   return (
---
+++ client/src/screens/Sock/Sock.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import './Sock.css'
+import Layout from '../../components/Layout/Layout'
+import { Link, useParams } from 'react-router-dom'
+import {getSock, deleteSock } from '../../services/socks'
+
+export default function Sock(props) {
+  const [sock, setSock] = useState(null)
+  const [isLoaded, setLoaded] = useState(false)
+    const { id } = useParams()
+  
+    useEffect(() => {
+      const fetchSock = async () => {
+        const sock = await getSock(id)
+        setSock(sock)
+        setLoaded(true)
+      }
+      fetchSock()
+    }, [id])
+  
+    if (!isLoaded) {
+      return <h1>Loading...</h1>
+    }
+  
+    return (
+      <Layout user={props.user}>
+        <div className='sock-detail'>
+          <img
+            className='sock-detail-image'
+            src={sock.imgURL}
+            alt={sock.name}
+          />
+          <div className='detail'>
+            <div className='name'>{sock.name}</div>
+            <div className='price'>{`$${sock.price}`}</div>
+            <div className='description'>{sock.description}</div>
+            <div className='sock1'>{sock.sock1}</div>
+            <div className='sock2'>{sock.sock2}</div>
+            <div className='button-container'>
+              <Link className='edit-button' to={`/socks/${sock._id}/edit`}>
+                Edit
+              </Link>
+              <button
+                className='delete-button'
+                onClick={() => deleteSock(sock._id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
