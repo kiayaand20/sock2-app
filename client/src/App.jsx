8a7d36b60326a27e227e07cfb7f41337d9d10421
Nitesh <nitlmisra@hotmail.com>
2022-01-04 14:24:33 -0500

    started signup component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import AllSocks from './screens/AllSocks/AllSocks'
 import AddSock from './screens/AddSock/AddSock'
 import Sock from './screens/Sock/Sock'
 import { Routes, Route } from 'react-router-dom'
+import SignUp from './screens/SignUp/SignUp'
 
 function App(props) {
   return (
@@ -15,6 +16,7 @@ function App(props) {
         <Route path='/socks' element={<AllSocks />} />
         <Route path='/new-arrivals' element={<AddSock />} />
         <Route path='/socks/:id' element={<Sock />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
     </div>
   )
