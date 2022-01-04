8fef87ff77b23e301c0a212077f0b22f151eed4b
Mayra and Oscar <mayraandoscar@mayras-mbp.attlocal.net>
2022-01-04 09:13:41 -0600

    resolve quote issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import "./App.css";
-import Home from "./screens/Home/Home";
-import AboutUs from "./screens/AboutUs/AboutUs";
-import AllSocks from "./screens/AllSocks/AllSocks";
-import { Routes, Route } from "react-router-dom";
+import './App.css';
+import Home from './screens/Home/Home';
+import AboutUs from './screens/AboutUs/AboutUs';
+import AllSocks from './screens/AllSocks/AllSocks';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="body">
+    <div className='body'>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/socks" element={<AllSocks />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/socks' element={<AllSocks />} />
       </Routes>
     </div>
   );
--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import './Layout.css'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import Nav from '../Nav/Nav'
+import React from 'react';
+import './Layout.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Nav from '../Nav/Nav';
 
 export default function Layout(props) {
   return (
-    <div className="layout">
+    <div className='layout'>
       <Header />
       <Nav user={props.user} />
-      <div className='layout-children'>
-        {props.children}
-      </div>
+      <div className='layout-children'>{props.children}</div>
       <Footer />
     </div>
   );
