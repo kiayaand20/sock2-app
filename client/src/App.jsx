80f087b32478c7e75174873e91f02f888deee91e
Nitesh <nitlmisra@hotmail.com>
2022-01-04 10:19:33 -0500

    saving changes to addsock screen and component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './screens/Home/Home';
 import AboutUs from './screens/AboutUs/AboutUs';
 import AllSocks from './screens/AllSocks/AllSocks';
+import AddSock from './screens/AddSock/AddSock';
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about-us' element={<AboutUs />} />
         <Route path='/socks' element={<AllSocks />} />
+        <Route path='/new-arrivals' element={ <AddSock />} />
       </Routes>
     </div>
   );
---
+++ client/src/screens/AddSock/AddSock.jsx
@@ -0,0 +1,10 @@
+import SockAdd from "../../components/Add/SockAdd";
+
+
+export default function AddSock() {
+  return (
+    <div>
+      <SockAdd/>
+    </div>
+  )
+}
