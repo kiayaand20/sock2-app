5ee1b502a5ca60f479dd084ec93ed35506c76f50
Akindele Akinfenwa <Dele@Akindeles-MacBook-Pro.local>
2022-01-06 14:19:08 -0500

    merge

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -27,6 +27,17 @@ const unauthenticated = (
 
 
 export default function Nav({ user }) {
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", mobileMenu);
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
   return (
     <nav>
       <div className='nav'>
@@ -35,6 +46,11 @@ export default function Nav({ user }) {
           {user && <div className="link-welcome">Welcome, {user.username}!</div>}
           {alwaysRender}
           {user ? authenticated : unauthenticated}
+        </div>
+        <div class="hamburger">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
         </div>
       </div>
 
--- client/src/screens/Sock/Sock.jsx
+++ client/src/screens/Sock/Sock.jsx
@@ -15,42 +15,42 @@ export default function Sock(props) {
       setSock(sock)
       setLoaded(true)
     }
-    
+
     fetchSock()
   }, [id])
 
   if (!isLoaded) {
     return <h1>Loading...</h1>
   }
-  
-    return (
-      <Layout user={props.user}>
-        <div className='sock-detail'>
-          <img
-            className='sock-detail-image'
-            src={sock.imgURL}
-            alt={sock.name}
-          />
-          <div className='detail'>
-            <div className='sock-name'>{sock.name}</div>
-            <div className='price'>{`$${sock.price}`}</div>
-            <div className='description'>{sock.description}</div>
-            <div className='sock1'>Sock 1: {sock.sock1}</div>
-            <div className='sock2'>Sock 2: {sock.sock2}</div>
-            <div className='button-container'>
-              <Link className='edit-button' to={`/socks/${sock._id}/edit`}>
-                <button className='edit-button'>
-                  Edit
-                </button>
-              </Link>
-              <Link to={'/'}>
-                <button
-                  className='delete-button'
-                  onClick={() => deleteSock(sock._id)}
-                >
-                    Delete
-                </button>
-              </Link>
+
+  return (
+    <Layout user={props.user}>
+      <div className='sock-detail'>
+        <img
+          className='sock-detail-image'
+          src={sock.imgURL}
+          alt={sock.name}
+        />
+        <div className='detail'>
+          <div className='sock-name'>{sock.name}</div>
+          <div className='price'>{`$${sock.price}`}</div>
+          <div className='description'>{sock.description}</div>
+          <div className='sock1'>Sock 1: {sock.sock1}</div>
+          <div className='sock2'>Sock 2: {sock.sock2}</div>
+          <div className='button-container'>
+            <Link className='edit-button' to={`/socks/${sock._id}/edit`}>
+              <button className='edit-button'>
+                Edit
+              </button>
+            </Link>
+            <Link className='delete-button' to={'/'}>
+              <button
+                className='delete-button'
+                onClick={() => deleteSock(sock._id)}
+              >
+                Delete
+              </button>
+            </Link>
           </div>
         </div>
       </div>
